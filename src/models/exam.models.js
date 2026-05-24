9ee0489fa560c2755f413138b11f0fb371b356b9
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-05-24 17:20:56 +0530

    feat: add exam model with schema and unique index for title, batch, and date

---
+++ src/models/exam.models.js
@@ -0,0 +1,40 @@
+const examSchema = new mongoose.Schema(
+  {
+    batch: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Batch",
+      required: true,
+    },
+
+    title: {
+      type: String, // "Class Test 1", "Unit Test - May"
+      required: true,
+    },
+
+    date: {
+      type: Date,
+      required: true,
+    },
+
+    totalMarks: {
+      type: Number,
+      required: true, // out of how much
+    },
+
+    passingMarks: {
+      type: Number,
+      required: true,
+    },
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true },
+);
+
+// prevent duplicate exam title for same batch on same date
+examSchema.index({ batch: true, title: true, date: true }, { unique: true });
+const Exam = mongoose.model("Exam", examSchema);
+export default Exam;
