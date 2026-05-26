e76248b2dec4eba4922e998edc657370410ad6b3
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-05-27 00:00:07 +0530

    feat: implement attendance creation functionality and update routes

--- src/controller/attendance.controller.js
+++ src/controller/attendance.controller.js
@@ -1,3 +1,4 @@
+import Attendance from "../models/attendance.models.js";
 import ApiError from "../utils/ApiError.utils.js";
 import asyncHandler from "../utils/asyncHandler.utils.js";
 
@@ -7,4 +8,19 @@ export const createAttendance = asyncHandler(async (req, res) => {
   if (!user) {
     throw new ApiError(400, "User not logged In.");
   }
+
+  if (user.role === "student") {
+    throw new ApiError(400, "Student can not set attendance.");
+  }
+
+  const { batch_id } = req.params;
+  const attendance = await Attendance.create({
+    batch,
+    student,
+    teacher,
+    date,
+    month,
+    status,
+    note,
+  });
 });
--- src/routes/attendance.routes.js
+++ src/routes/attendance.routes.js
@@ -1,11 +1,12 @@
-// import { Router } from "express";
+import { Router } from "express";
 
-// import { logInAuth } from "../middleware/logInAuth.middleware.js";
-// import authorizeRoles from "../middleware/authorizeRoles.middleware.js";
-// import { available_user_roles } from "../utils/constants.utils.js";
+import { logInAuth } from "../middleware/logInAuth.middleware.js";
+import authorizeRoles from "../middleware/authorizeRoles.middleware.js";
+import { available_user_roles } from "../utils/constants.utils.js";
+import { createAttendance } from "../controller/attendance.controller.js";
 
-// const attendanceRouter = Router();
+const attendanceRouter = Router();
 
-// attendanceRouter.post("/first-user", createFirstUser);
+attendanceRouter.post("/", createAttendance);
 
-// export default attendanceRouter;
+export default attendanceRouter;
