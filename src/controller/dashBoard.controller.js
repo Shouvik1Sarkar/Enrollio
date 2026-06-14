e123ebb9140d658f264e95d112bd8d46b35987be
Shouvik1Sarkar <shouvik3501@gmail.com>
2026-06-14 15:37:30 +0530

    feat: enhance admin dashboard to include overdue fees calculation and logging

--- src/controller/dashBoard.controller.js
+++ src/controller/dashBoard.controller.js
@@ -10,15 +10,17 @@ export const adminDashBoard = asyncHandler(async (req, res) => {
   if (!user) {
     throw new ApiError(401, "User Not Logged In.");
   }
-
+  /****************** Number of Batches ******************/
   const allBatches = await Batch.find();
 
   const batch_number = allBatches.length;
 
+  /****************** Number of Students ******************/
   const all_students = await Student.find();
 
   const student_numbers = all_students.length;
 
+  /****************** Total Pending amount and pending fees ******************/
   const pending_accounts_array = all_students.map((x) => {
     const r = x.feeHistory.filter((a) => a.status == "pending");
     return r;
@@ -40,15 +42,35 @@ export const adminDashBoard = asyncHandler(async (req, res) => {
   );
   console.log("=======> ", total_pending_amount);
 
-  //   const fee_record = all_students.map((x) => x.feeHistory);
+  /****************** TotalOver Due amount and pending fees ******************/
+  const overDue_accounts_array = all_students.map((x) => {
+    const r = x.feeHistory.filter((a) => a.status == "overdue");
+    return r;
+  });
+  const overDue_amount = overDue_accounts_array.flat(Infinity);
 
-  //   console.log("PENDING AMOUNT => ", pending_amount.flat(Infinity));
+  console.log("OVER-DUE AMOUNT => ", pending_amount.flat(Infinity));
+
+  const overDue_amount_count = overDue_amount.length;
+
+  const total_overDue_amount_array = overDue_amount.map((s) => {
+    return s.amount;
+  });
+  console.log("=======> ", total_overDue_amount_array);
+
+  const total_overDue_amount = total_overDue_amount_array.reduce(
+    (acc, num) => acc + num,
+    0,
+  );
+  console.log("=======> ", total_overDue_amount);
 
   const result_1 = {
     total_batches: batch_number,
     total_students: student_numbers,
     pending_amount_count,
     total_pending_amount,
+    overDue_amount_count,
+    total_overDue_amount,
   };
 
   const result = { result_1 };
